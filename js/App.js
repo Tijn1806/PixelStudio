07bf91bff935aef18e9f6f4bdb94a95f6e104e22
Tijn1806 <Tijn@kromjong.nl>
2026-06-26 14:07:05 +0200

    Render functie

--- js/App.js
+++ js/App.js
@@ -1,1 +1,14 @@
+import Grid from "./Grid.js";
 
+export default class App {
+  constructor(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext("2d");
+
+    this.grid = new Grid(this.canvas.width, this.canvas.height, 25);
+  }
+
+  start() {
+    this.grid.render(this.ctx);
+  }
+}
