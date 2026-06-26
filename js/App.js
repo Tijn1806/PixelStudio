081971a56bb5c4eedf1b3674f52225f67b606bd2
Tijn1806 <Tijn@kromjong.nl>
2026-06-26 16:25:20 +0200

    App.js aangepast

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 import Grid from "./Grid.js";
 import PenTool from "./PenTool.js";
+import EraserTool from "./EraserTool.js";
 
 export default class App {
   constructor(canvasId) {
@@ -7,9 +8,15 @@ export default class App {
     this.ctx = this.canvas.getContext("2d");
 
     this.grid = new Grid(this.canvas.width, this.canvas.height, 25);
+
     this.penTool = new PenTool("#000000");
+    this.eraserTool = new EraserTool();
+    this.activeTool = this.penTool;
 
     this.colorPicker = document.getElementById("colorPicker");
+    this.penButton = document.getElementById("penButton");
+    this.eraserButton = document.getElementById("eraserButton");
+    this.clearButton = document.getElementById("clearButton");
   }
 
   start() {
@@ -24,6 +31,20 @@ export default class App {
 
     this.colorPicker.addEventListener("input", event => {
       this.penTool.setColor(event.target.value);
+      this.activeTool = this.penTool;
+    });
+
+    this.penButton.addEventListener("click", () => {
+      this.activeTool = this.penTool;
+    });
+
+    this.eraserButton.addEventListener("click", () => {
+      this.activeTool = this.eraserTool;
+    });
+
+    this.clearButton.addEventListener("click", () => {
+      this.grid.clear();
+      this.grid.render(this.ctx);
     });
   }
 
@@ -35,7 +56,7 @@ export default class App {
 
     const pixel = this.grid.getPixelAt(mouseX, mouseY);
 
-    this.penTool.use(pixel);
+    this.activeTool.use(pixel);
     this.grid.render(this.ctx);
   }
 }
