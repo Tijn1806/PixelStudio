c41befff7a6d58e42d157d4ca1823a40c8a8d360
Tijn1806 <Tijn@kromjong.nl>
2026-06-26 14:06:22 +0200

    Grid class aangemaakt

--- js/Grid.js
+++ js/Grid.js
@@ -1,1 +1,27 @@
+import Pixel from "./Pixel.js";
 
+export default class Grid {
+  constructor(width, height, pixelSize) {
+    this.width = width;
+    this.height = height;
+    this.pixelSize = pixelSize;
+    this.pixels = [];
+
+    this.createPixels();
+  }
+
+  createPixels() {
+    for (let y = 0; y < this.height; y += this.pixelSize) {
+      for (let x = 0; x < this.width; x += this.pixelSize) {
+        const pixel = new Pixel(x, y, this.pixelSize);
+        this.pixels.push(pixel);
+      }
+    }
+  }
+
+  render(ctx) {
+    this.pixels.forEach(pixel => {
+      pixel.draw(ctx);
+    });
+  }
+}
