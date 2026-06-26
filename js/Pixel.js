4d0957b6d48862b510aaf9faefa880943d93fcd5
Tijn1806 <Tijn@kromjong.nl>
2026-06-26 14:05:49 +0200

    Pixel class aangemaakt

--- js/Pixel.js
+++ js/Pixel.js
@@ -1,1 +1,16 @@
+export default class Pixel {
+  constructor(x, y, size, color = "#ffffff") {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
 
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+
+    ctx.strokeStyle = "#cccccc";
+    ctx.strokeRect(this.x, this.y, this.size, this.size);
+  }
+}
